7d3d1de610c53320ad03bb6bea525fff49a7fe20
khwww <khw4420@knu.ac.kr>
2024-07-02 01:02:59 +0900

    ui제작 및 반응형구현

--- main.js
+++ main.js
@@ -2,8 +2,17 @@ let computerNum = 0;
 let playbutton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 3;
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area");
+let history = [];
 
 playbutton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus", function () {
+  userInput.value = "";
+});
 
 function pickRandomNum() {
   computerNum = Math.floor(Math.random() * 100) + 1;
@@ -12,6 +21,19 @@ function pickRandomNum() {
 
 function play() {
   let userValue = userInput.value;
+
+  if (userValue < 1 || userValue > 100) {
+    resultArea.textContent = "1과100사이의 숫자를 입력해주세요";
+    return;
+  }
+
+  if (history.includes(userValue)) {
+    resultArea.textContent = "이미 입력한 숫자입니다 다른 숫자를 입력해주세요";
+    return;
+  }
+  chances--;
+  chanceArea.textContent = `남은 기회: ${chances}번`;
+  console.log("chance", chances);
   if (userValue < computerNum) {
     resultArea.textContent = "UP!";
     // console.log("UP!");
@@ -20,8 +42,23 @@ function play() {
     // console.log("DOWN!");
   } else {
     resultArea.textContent = "정답입니다!";
-    // console.log("정답입니다~");
+    gameOver = true;
   }
+
+  history.push(userValue);
+
+  if (chances < 1) {
+    gameOver = true;
+  }
+  if (gameOver == true) {
+    playbutton.disabled = true;
+  }
+}
+
+function reset() {
+  userInput.value = "";
+  pickRandomNum();
+  resultArea.textContent = "결과값이 여기에 표시됩니다";
 }
 
 pickRandomNum();
