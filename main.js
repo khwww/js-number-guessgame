9e2443ed116a5cbc98103886067bf72c6ea16585
khwww <khw4420@knu.ac.kr>
2024-07-02 23:52:20 +0900

    reset 후 게임진행

--- main.js
+++ main.js
@@ -20,26 +20,26 @@ function pickRandomNum() {
 }
 
 function play() {
-  let userValue = userInput.value;
+  let userValue = parseInt(userInput.value); // 숫자로 변환
 
   if (userValue < 1 || userValue > 100) {
-    resultArea.textContent = "1과100사이의 숫자를 입력해주세요";
+    resultArea.textContent = "1과 100 사이의 숫자를 입력해주세요";
     return;
   }
 
   if (history.includes(userValue)) {
-    resultArea.textContent = "이미 입력한 숫자입니다 다른 숫자를 입력해주세요";
+    resultArea.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력해주세요";
     return;
   }
+
   chances--;
   chanceArea.textContent = `남은 기회: ${chances}번`;
   console.log("chance", chances);
+
   if (userValue < computerNum) {
     resultArea.textContent = "UP!";
-    // console.log("UP!");
   } else if (userValue > computerNum) {
     resultArea.textContent = "DOWN!";
-    // console.log("DOWN!");
   } else {
     resultArea.textContent = "정답입니다!";
     gameOver = true;
@@ -50,6 +50,7 @@ function play() {
   if (chances < 1) {
     gameOver = true;
   }
+
   if (gameOver == true) {
     playbutton.disabled = true;
   }
@@ -59,6 +60,11 @@ function reset() {
   userInput.value = "";
   pickRandomNum();
   resultArea.textContent = "결과값이 여기에 표시됩니다";
+  chances = 3;
+  gameOver = false;
+  chanceArea.textContent = `남은 기회: ${chances}번`;
+  history = [];
+  playbutton.disabled = false;
 }
 
 pickRandomNum();
